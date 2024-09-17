f3303b29b59ea800856709827ec57ad1d4c0ed2b
Hikari681 <quanghoang681@gmail.com>
2024-09-17 20:21:24 +0700

    test

--- video2/video2.js
+++ video2/video2.js
@@ -1,5 +1,29 @@
-let a = 'toi la string'
-let text = `We are the so-called "Vikings" from the north.`;
-console.log(text)
-console.log(a)
-console.log(4 + 5 + 'ab')
+console.log('Hello word form AA')
+
+// //key: value
+// let obj = {
+//     name: 'Quang anh', address: 'Thai Binh'
+// };
+
+// console.log('check data obj: ', obj)
+// console.log('what is your name?', `My name is:`, obj.name)
+// // console.log('what is your name?', 'My name is:', obj['name'])
+
+let arrTop4 = ['Ha Noi', 'HCM', 'DN', 'TB']
+// for (let i = 0; i < arrTop4.length; i++) {
+//     console.log('Top: ', i + 1, arrTop4[i])
+// }
+// for (let i = 0; i < 10; i++) {
+//     console.log('check validate i=', i)
+// }
+
+let i = 0
+while (i < arrTop4.length) {
+    console.log('Top: ', i + 1, arrTop4[i]); i++;
+}
+
+let flag = false;
+while (!flag) {
+    console.log('Top: ', i); i++;
+    if (i === 10) flag = true;
+}
