95aa30c85dbb72a59f7401829a5e5650c10037fd
Hikari681 <quanghoang681@gmail.com>
2024-09-29 19:07:47 +0700

    add new

--- video2/video2.js
+++ video2/video2.js
@@ -9,21 +9,42 @@ console.log('Hello word form AA')
 // console.log('what is your name?', `My name is:`, obj.name)
 // // console.log('what is your name?', 'My name is:', obj['name'])
 
-let arrTop4 = ['Ha Noi', 'HCM', 'DN', 'TB']
-// for (let i = 0; i < arrTop4.length; i++) {
-//     console.log('Top: ', i + 1, arrTop4[i])
-// }
-// for (let i = 0; i < 10; i++) {
-//     console.log('check validate i=', i)
-// }
+let arrTop4 = ['Ha Noi', 'HCM', 'DN1', 'TB2']
+// // for (let i = 0; i < arrTop4.length; i++) {
+// //     console.log('Top: ', i + 1, arrTop4[i])
+// // }
+// // for (let i = 0; i < 10; i++) {
+// //     console.log('check validate i=', i)
+// // }
 
-let i = 0
+// // let i = 0
+// // while (i < arrTop4.length) {
+// //     console.log('Top: ', i + 1, arrTop4[i]); i++;
+// // }
+
+// // let flag = false;
+// // while (!flag) {
+// //     console.log('Top: ', i); i++;
+// //     if (i === 10) flag = true;
+// // }
+
+let i = 0;
 while (i < arrTop4.length) {
-    console.log('Top: ', i + 1, arrTop4[i]); i++;
+    i++;
+    if (arrTop4[i] === 'HCM') {
+        console.log('Top:', i + 1, arrTop4[i]);
+        continue
+    }
 }
+// let citiesTop4 = ['Ha Noi', 'HCM3', 'DN1', 'TB2'];
 
-let flag = false;
-while (!flag) {
-    console.log('Top: ', i); i++;
-    if (i === 10) flag = true;
-}
+// for (let i = 0; i < citiesTop4.length; i++) {
+//     const city = citiesTop4[i];
+//     const cityLength = city.length;
+
+//     if (cityLength === 2 || cityLength === 4) {
+//         console.log('Top:', i + 1, city);
+//     } else {
+//         console.log('Top:', i + 1, 'No data');
+//     }
+// }    
