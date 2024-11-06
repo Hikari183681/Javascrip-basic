19cc706e5ed53989de3bce3619654b998fb1ff38
Hikari681 <quanghoang681@gmail.com>
2024-11-06 20:21:51 +0700

    video8

---
+++ video8/video8.js
@@ -0,0 +1,19 @@
+console.log('Hello word form AA')
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// for (let i = 0; i < arr.length; i++) {
+//     arr[i] = arr[i] * arr[i];
+// }
+
+// let mapArr = arr.map((item, index) => {
+//     item = item * item;
+//     return item;
+// })
+
+// console.log('check value arr:', arr)
+// console.log('check value mapArr:', mapArr)
+
+const array1 = [1, 30, 4, 21, 100000];
+array1.sort();
+console.log(array1);
+// Expected output: Array [1, 100000, 21, 30, 4]
